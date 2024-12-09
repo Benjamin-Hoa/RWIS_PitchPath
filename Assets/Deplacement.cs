faa64a8d758ad4e82fe68e8a6479a4b9faf871db
Benjamin Hoa <benjamin.hoa@eleves.ec-nantes.fr>
2024-12-09 10:03:42 +0100

    project creation

---
+++ Assets/Deplacement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deplacement : MonoBehaviour
+{
+    float ratio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetHeight(float pitch)
+    {
+        float height = CvtPitchToHeight(pitch);
+
+
+        float position_y = gameObject.transform.position.y;
+        position_y = height;
+    }
+
+    private float CvtPitchToHeight(float pitch)
+    {
+        return (ratio * pitch);
+    }
+}
