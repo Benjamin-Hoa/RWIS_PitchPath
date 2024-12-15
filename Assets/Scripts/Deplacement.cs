9af9c021ddbb230fa47d4d5887fbd6ba6583da6c
Benjamin Hoa <benjamin.hoa@eleves.ec-nantes.fr>
2024-12-15 11:58:16 +0100

    Making keyboard moving available for debug

--- Assets/Scripts/Deplacement.cs
+++ Assets/Scripts/Deplacement.cs
@@ -15,7 +15,18 @@ public class Deplacement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y--;
+            this.transform.position = position;
+        }
     }
 
     public void SetHeight(float pitch)
