73c86316910f94477abf204ee32afd68e587f38a
LAPTOP-HF87TK0J\PC <hanlaassilia2003@gmail.com>
2025-01-12 11:02:35 +0900

    Add the start menu

---
+++ Assets/Scripts/InstructionsController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class InstructionsController : MonoBehaviour
+{
+    public GameObject instructionsPanel;
+
+    // Show the instructions
+    public void ShowInstructions()
+    {
+        instructionsPanel.SetActive(true);
+    }
+
+    // Hide the instructions
+    public void HideInstructions()
+    {
+        instructionsPanel.SetActive(false);
+    }
+}
