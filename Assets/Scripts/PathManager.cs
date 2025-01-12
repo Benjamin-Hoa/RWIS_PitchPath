49153630d063c3c53b3fdd3696d04eb273837e56
Benjamin Hoa <benjamin.hoa@eleves.ec-nantes.fr>
2025-01-12 08:31:16 +0100

    added pitch choice

--- Assets/Scripts/PathManager.cs
+++ Assets/Scripts/PathManager.cs
@@ -10,6 +10,8 @@ public class PathManager : MonoBehaviour
     float deltatime = 0; //The minimum time elapsed between two notes
     List<float> notes = new List<float>();//The list of succeeding notes frequency, all separated by deltatime
     List<int> notesPos = new List<int>();//position of note from 0 to 11
+    float[] notes_freq_high = { (float)164.81, (float)174.61, (float)184.99, (float)195.99, (float)207.65, (float)220, (float)233.08, 246.94f, 261.52f, 277.18f, 293.66f, 311.12f };//base notes frequencies
+    float[] notes_freq_low = { (float)164.81, (float)174.61, (float)184.99, (float)195.99, (float)207.65, (float)220, (float)233.08, 246.94f, 261.52f, 277.18f, 293.66f, 311.12f };//base notes frequencies
     float[] notes_freq = {(float)164.81, (float)174.61, (float)184.99, (float)195.99, (float)207.65, (float)220, (float)233.08,246.94f,261.52f,277.18f,293.66f,311.12f};//base notes frequencies
     [SerializeField] GameObject NotePoint_prefab;//Prefab to be able to spawn notepoints
     int nbNotes = 10; //number of notes
